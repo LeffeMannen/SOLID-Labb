15c24e1c744f3a70805fb1785f2f9fc86b1c26e5
LeffeMannen <andreas.ohlsson@elev.ga.ntig.se>
2021-01-22 15:04:37 +0100

    e

---
+++ SOLID-Labb/SOLID-Labb/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SOLID_Labb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal[] animals = ColorAnimal();
+
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+
+            foreach (var animal in animals)
+            {
+                TextAnimal(animal);
+            }
+
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+
+        }
+
+        static Animal[] ColorAnimal()
+        {
+            Animal[] animals = {new Dog("Black"),
+                            new Hedgehog("Pink"),
+                            new Bird("Yellow")};
+
+            return animals;
+        }
+
+        static void TextAnimal(Animal animal)
+        {
+            if (animal is Dog dog)
+            {
+                dog.TransferOwnership("Niklas");
+            }
+
+            animal.Eat();
+            animal.Sleep();
+            animal.Speak();
+            animal._color = "Grey";
+            Console.WriteLine(animal.GetType().Name + " is " + animal._color);
+            Console.WriteLine();
+
+        }
+    }
+
+
+}
